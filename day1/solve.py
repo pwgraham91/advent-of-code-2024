a83f219963f97051b7a217afe50aa17242dfa88b
petergraham <pwgraham91@gmail.com>
2024-12-04 21:34:53 -0600

    day 1

--- day1/solve.py
+++ day1/solve.py
@@ -1,4 +1,4 @@
-def run():
+def runPart1():
     left, right = get_columns()
     sorted_left = sorted(left)
     sorted_right = sorted(right)
@@ -12,14 +12,29 @@ def run():
         difference += max_i - min_i
     print(difference)
 
+def runPart2():
+    left, right = get_columns()
+    right_count = {}
+    for i in right:
+        if right_count.get(i) == None:
+            right_count[i] = 1
+        else:
+            right_count[i] += 1
+
+    counter = 0
+    for i in left:
+        counter += i * right_count.get(i, 0)
+
+    print(counter)
+
 def get_columns():
     left = []
     right = []
-    with open("input1.txt", "r") as file:
+    with open("input.txt", "r") as file:
         for line in file:
             x, y = line.split()
             left.append(int(x))
             right.append(int(y))
     return left, right
 
-run()
+runPart2()
